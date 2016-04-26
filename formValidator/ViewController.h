87b3a6d5eaa69f9b80415b224b59eac116137c9e
Allen Spicer <allenspicer@gmail.com>
2016-04-26 10:46:59 -0400

    Initial Commit

---
+++ formValidator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  formValidator
+//
+//  Created by Allen Spicer on 4/26/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
